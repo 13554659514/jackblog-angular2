bf5418410dd6faeadcb9e3d8f9465e61fe4566da
Jack Hu <hutufeia2009@sina.com>
2016-11-24 20:09:00 +0800

    add toaster

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,30 @@
-import { NgModule, ApplicationRef } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { removeNgStyles, createNewHosts, createInputTransfer } from '@angularclass/hmr';
-import { DropdownModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { NgModule, ApplicationRef } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
+import { RouterModule } from '@angular/router'
+import { removeNgStyles, createNewHosts, createInputTransfer } from '@angularclass/hmr'
+//第三方模块
+import { DropdownModule } from 'ng2-bootstrap/ng2-bootstrap'
+import { ToasterModule } from 'angular2-toaster/angular2-toaster'
 /*
  * Platform and Environment providers/directives/pipes
  */
-import { ENV_PROVIDERS } from './env';
-import { ROUTES } from './app.routes';
+import { ENV_PROVIDERS } from './env'
+import { ROUTES } from './app.routes'
 
 // import { APP_RESOLVER_PROVIDERS } from './app.resolver';
-import { AppState, InternalStateType } from './app.service';
+import { AppState, InternalStateType } from './app.service'
 
 // App is our top level component
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/Home';
+import { AppComponent } from './app.component'
+import { HomeComponent } from './components/Home'
 import { CustomTimePipe,FormatDatePipe } from './pipes'
+//css
+import 'font-awesome/css/font-awesome.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jackblog-sass/dist/index.css'
+import './assets/styles/index.css'
 
 // Application wide providers
 const APP_PROVIDERS = [
@@ -47,6 +54,7 @@ type StoreType = {
     FormsModule,
     HttpModule,
     DropdownModule,
+    ToasterModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
