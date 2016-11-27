ad645d0349f910ff56392a074ad5e9978a50435e
Jack Hu <hutufeia2009@sina.com>
2016-11-27 22:44:53 +0800

    add drowpdown

--- src/modules/Navbar/index.ts
+++ src/modules/Navbar/index.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core'
 import { HttpModule } from '@angular/http'
 import { FormsModule } from '@angular/forms'
 import { Routes, RouterModule } from '@angular/router'
-import { DropdownModule,ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { DropdownModule } from 'ng2-bootstrap/ng2-bootstrap'
 
 import NavbarComponent from './navbar.component'
-// import TagsComponent from './tags.component'
-// import ArticleListComponent from './articles.component'
-// import LoadMoreComponent from './loadmore.component'
-// import FooterComponent from './footer.component'
-//import { TodoService } from './todo.service';
-//import { MyTodoResolver } from './todo.resolver';
-//import { MyTodoGuard, CanLeaveTodoDetailGuard } from './todo.guards';
-//import { TagService } from '../../services'
 
 @NgModule({
-  imports: [ CommonModule,RouterModule,HttpModule ],
+  imports: [ CommonModule,RouterModule,HttpModule,DropdownModule ],
   declarations: [
     NavbarComponent
   ],
